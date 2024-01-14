5a8ba2a2e1979eb3eb1db557dd4ed9ad12314ef9
Станислав <134084102+bazonly@users.noreply.github.com>
2024-01-14 23:23:59 +0600

    Update tests_point_2.py

--- tests_point_2.py
+++ tests_point_2.py
@@ -5,3 +5,5 @@ from distance import Millimeter, Centimeter, Meter, Inch
 def test_repr_method():
     instance = Inch(9.2332)
     assert repr(instance) in ['Inch(value=9.2332)', 'Inch(9.2332)'], 'Метод __repr__ представляет некорректную или недостаточно информации об объекте'
+
+test_repr_method()
